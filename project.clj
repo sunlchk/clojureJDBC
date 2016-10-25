5934dd7882b2710eac1ed75432f7a0de3e2cb7b4
Sunil <Sunil Gopinath>
2016-10-25 17:38:17 +0530

    jdbc commands

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject mysql "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.6.0"]
+;                 [org.clojure/java.jdbc "0.0.6"]     
+                 [org.clojure/java.jdbc "0.6.2-alpha3"]
+                 [mysql/mysql-connector-java "5.1.39"]])
